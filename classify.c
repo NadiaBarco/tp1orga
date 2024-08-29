37fea08d2bf3c8d12fface75e7a82a1632aafdae
NadiaBarco <48421521+NadiaBarco@users.noreply.github.com>
2024-08-29 16:54:32 -0300

    Create classify.c

---
+++ classify.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct classifier_s {
+    char** vowels_and_consonants;
+	char* string;
+} classifier_t;
+
+
+void classify_chars_in_string(char* string, char** vowels_and_cons) {
+    
+    while(string!='/0'){
+        if (*string<= )    
+        ++string;
+    } 
+}
+
+void classify_chars(classifier_t* array, uint64_t size_of_array) {
+}
+
+
+
+int main()
+{
+    
+    return 0;
+}
