46056d0ecaa47cdeec55301c802d5a0046e6180d
NadiaBarco <48421521+NadiaBarco@users.noreply.github.com>
2024-08-29 22:08:51 -0300

    Update classify.c
    seguir

--- classify.c
+++ classify.c
@@ -11,11 +11,18 @@ typedef struct classifier_s {
 
 
 void classify_chars_in_string(char* string, char** vowels_and_cons) {
-    
+    char vowels[];
     while(string!='/0'){
-        if (*string<= )    
-        ++string;
-    } 
+        if(Aa<=*string<=Zz){
+            if (*string=='a'|| *string=='e'||*string=='i'||*string=='o'||*string=='u' ){    
+                
+            }else{
+                
+            }
+            
+        } 
+    ++string;
+    
 }
 
 void classify_chars(classifier_t* array, uint64_t size_of_array) {
@@ -28,3 +35,4 @@ int main()
     
     return 0;
 }
+
